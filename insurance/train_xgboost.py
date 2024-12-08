e6a0accc1d161948ef7ee8cab0273f1d7f2f70af
Jonathan Faustin <faustin.jonathan@gmail.com>
2024-12-08 10:42:39 -0500

    refactor data pipeline

--- insurance/train_xgboost.py
+++ insurance/train_xgboost.py
@@ -4,21 +4,19 @@ from pathlib import Path
 import dvc.api
 import numpy as np
 import pandas as pd
+import xgboost as xgb
 from dvclive import Live
 from sklearn.base import clone
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.metrics import root_mean_squared_log_error
 from sklearn.model_selection import KFold
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, OrdinalEncoder, StandardScaler
-from xgboost import XGBRegressor
 
 from insurance.common import OUT_PATH, PREP_DATA_PATH
+from insurance.data_pipeline import make_pipeline
 
 model_label = Path(__file__).stem.split("_")[-1]
 
 MODEL_PATH = OUT_PATH / f"models/model_{model_label}.pkl"
+DATA_PIPELINE_PATH = MODEL_PATH.parent / f"data_pipeline_{model_label}.pkl"
 MODEL_PATH.parent.mkdir(parents=True, exist_ok=True)
 
 
@@ -30,71 +28,8 @@ def main():
     df = pd.read_csv(PREP_DATA_PATH / "prepared_data.csv")
     features = df.drop(columns=[target_column])
     labels = df[target_column]
-    numeric_features = [
-        "Age",
-        "Number of Dependents",
-        "Vehicle Age",
-        "Previous Claims",
-        "Health Score",
-        "Credit Score",
-        "Insurance Duration",
-    ]
-    numeric_log_features = ["Annual Income"]
-    categorical_features = features.select_dtypes(include=["object", "category"]).columns.tolist()
-    ordinal_features = []
 
-    feat_cols = numeric_features + numeric_log_features + categorical_features + ordinal_features
-
-    # Preprocessing pipeline
-    numeric_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="mean")),
-            ("scaler", StandardScaler()),
-        ]
-    )
-    log_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="median")),
-            ("log", FunctionTransformer(np.log1p, validate=True)),
-            ("scaler", StandardScaler()),
-        ]
-    )
-
-    oh_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("onehot", OneHotEncoder(handle_unknown="ignore")),
-        ]
-    )
-
-    ord_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("ordinal", OrdinalEncoder()),
-        ]
-    )
-
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ("num", numeric_transformer, numeric_features),
-            (
-                "num_log",
-                log_transformer,
-                numeric_log_features,
-            ),
-            ("oh", oh_transformer, categorical_features),
-            ("ord", ord_transformer, ordinal_features),
-        ],
-        remainder="drop",
-    )
-
-    model = XGBRegressor(**params["xgboost"])
-    pipeline = Pipeline(
-        [
-            ("preprocessor", preprocessor),
-            ("model", model),
-        ]
-    )
+    data_pipeline, feat_cols = make_pipeline(features=features)
 
     # Stratified K-Fold Cross-Validation
     n_splits = params["n_splits"]
@@ -111,10 +46,14 @@ def main():
             y_train, y_test = labels.iloc[train_idx], labels.iloc[test_idx]
             print(f"{X_train.shape=}")
             # Fit the pipeline
-            pipeline.fit(X_train, np.log1p(y_train))
+            X_train = data_pipeline.fit_transform(X_train)
+            dtrain = xgb.DMatrix(X_train, label=np.log1p(y_train))
+            bst = xgb.train(params["xgboost"], dtrain)
 
             # Predict and evaluate
-            y_pred = np.expm1(pipeline.predict(X_test))
+            X_test = data_pipeline.transform(X_test)
+            dvalid = xgb.DMatrix(X_test, label=np.log1p(y_test))
+            y_pred = np.expm1(bst.predict(dvalid))
             rmsle = root_mean_squared_log_error(y_test, y_pred)
             rmsle_scores.append(rmsle)
             print(f"Root Mean Squared Logarithmic Error: {rmsle:.4f}")
@@ -131,9 +70,12 @@ def main():
     # Re-train on entire dataset
     X_train = features[feat_cols]
     y_train = labels
-    re_pipeline = clone(pipeline)
-    re_pipeline.fit(X_train, np.log1p(y_train))
-    pickle.dump(re_pipeline, (MODEL_PATH).open("wb"))
+    re_pipeline = clone(data_pipeline)
+    X_train = re_pipeline.fit_transform(X_train)
+    dtrain = xgb.DMatrix(X_train, label=np.log1p(y_train))
+    bst = xgb.train(params["xgboost"], dtrain)
+    pickle.dump(re_pipeline, (DATA_PIPELINE_PATH).open("wb"))
+    pickle.dump(bst, MODEL_PATH.open("wb"))
     live.log_artifact(MODEL_PATH)
 
 
--- insurance/tune_xgboost.py
+++ insurance/tune_xgboost.py
@@ -1,20 +1,17 @@
 import pickle
 from pathlib import Path
 
-import dvc.api
 import numpy as np
 import optuna
 import pandas as pd
+import xgboost as xgb
 from dvclive.optuna import DVCLiveCallback
-from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
 from sklearn.metrics import root_mean_squared_log_error
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, OrdinalEncoder, StandardScaler
 from xgboost import XGBRegressor
 
 from insurance.common import OUT_PATH, PREP_DATA_PATH
+from insurance.data_pipeline import make_pipeline
 
 model_label = Path(__file__).stem.split("_")[-1]
 
@@ -32,69 +29,8 @@ def main():
     df = pd.read_csv(PREP_DATA_PATH / "prepared_data.csv")
     features = df.drop(columns=[target_column])
     labels = df[target_column]
-    numeric_features = [
-        "Age",
-        "Number of Dependents",
-        "Vehicle Age",
-        "Previous Claims",
-        "Health Score",
-        "Credit Score",
-        "Insurance Duration",
-    ]
-    numeric_log_features = ["Annual Income"]
-    categorical_features = features.select_dtypes(include=["object", "category"]).columns.tolist()
-    ordinal_features = []
 
-    feat_cols = numeric_features + numeric_log_features + categorical_features + ordinal_features
-
-    # Preprocessing pipeline
-    numeric_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="mean")),
-            ("scaler", StandardScaler()),
-        ]
-    )
-    log_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="median")),
-            ("log", FunctionTransformer(np.log1p, validate=True)),
-            ("scaler", StandardScaler()),
-        ]
-    )
-
-    oh_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("onehot", OneHotEncoder(handle_unknown="ignore")),
-        ]
-    )
-
-    ord_transformer = Pipeline(
-        [
-            ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("ordinal", OrdinalEncoder()),
-        ]
-    )
-
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ("num", numeric_transformer, numeric_features),
-            (
-                "num_log",
-                log_transformer,
-                numeric_log_features,
-            ),
-            ("oh", oh_transformer, categorical_features),
-            ("ord", ord_transformer, ordinal_features),
-        ],
-        remainder="drop",
-    )
-
-    data_pipeline = Pipeline(
-        [
-            ("preprocessor", preprocessor),
-        ]
-    )
+    data_pipeline, feat_cols = make_pipeline(features=features)
     pickle.dump(data_pipeline, (DATA_PIPELINE_PATH).open("wb"))
 
     def objective(trial):
@@ -102,11 +38,15 @@ def main():
             features[feat_cols], labels, test_size=0.25
         )
         X_train = data_pipeline.fit_transform(X_train)
+        dtrain = xgb.DMatrix(X_train, label=np.log1p(y_train))
+        X_test = data_pipeline.transform(X_test)
+        dvalid = xgb.DMatrix(X_test, label=np.log1p(y_test))
 
         param = {
             "device": "cuda",
             "verbosity": 0,
             "objective": "reg:squarederror",
+            "random_state": 42,
             # use exact for small dataset.
             "tree_method": "auto",
             # defines booster, gblinear for linear functions.
@@ -141,16 +81,16 @@ def main():
             param["rate_drop"] = trial.suggest_float("rate_drop", 1e-8, 1.0, log=True)
             param["skip_drop"] = trial.suggest_float("skip_drop", 1e-8, 1.0, log=True)
 
-        model = XGBRegressor(**param)
-        model.fit(X_train, np.log1p(y_train))
+        bst = xgb.train(param, dtrain)
 
-        y_pred = np.expm1(model.predict(data_pipeline.transform(X_test)))
+        # Predict and evaluate
+        y_pred = np.expm1(bst.predict(dvalid))
         rmsle = root_mean_squared_log_error(y_test, y_pred)
         print(f"Root Mean Squared Logarithmic Error: {rmsle:.4f}")
         return rmsle
 
     study = optuna.create_study(direction="minimize")
-    study.optimize(objective, n_trials=100, callbacks=[DVCLiveCallback(str(TUNE_PATH))])
+    study.optimize(objective, n_trials=120, callbacks=[DVCLiveCallback(str(TUNE_PATH))])
 
     print("Number of finished trials: ", len(study.trials))
     print("Best trial:")
