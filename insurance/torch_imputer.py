00bc8b8b883bfff825292150278466f96359f131
Jonathan Faustin <faustin.jonathan@gmail.com>
2024-12-10 23:00:10 -0500

    first version of imputer trained with torch on 3 features

--- insurance/torch_imputer.py
+++ insurance/torch_imputer.py
@@ -15,7 +15,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import FunctionTransformer, OneHotEncoder, OrdinalEncoder, StandardScaler
 from torch.utils.data import DataLoader, Dataset
-from sklearn.metrics import mean_squared_error
+from tqdm import tqdm
 
 
 @dataclass
@@ -133,7 +133,7 @@ def train(prepared_data_path: Path):
     features_columns = Features(
         numeric=[
             # "Age",
-            # "Health Score",
+            "Health Score",
             "Credit Score",
             # "Insurance Duration",
             # "Number of Dependents",
@@ -146,12 +146,13 @@ def train(prepared_data_path: Path):
 
     data_pipeline = make_pipeline(features=features_columns)
     X_train = data_pipeline.fit_transform(X_train)
+    X_test = data_pipeline.transform(X_test)
 
     print("Transformed trained data")
 
     # Create PyTorch Datasets and DataLoaders
     train_dataset = TabularDataset(X_train, y_train.values)
-    test_dataset = TabularDataset(X_test, y_test)
+    test_dataset = TabularDataset(X_test, y_test.values)
 
     train_loader = DataLoader(train_dataset, batch_size=params["batch_size"], shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=params["batch_size"], shuffle=False)
@@ -159,6 +160,7 @@ def train(prepared_data_path: Path):
     print("Pytorch data structures")
 
     # Initialize the model, loss function, and optimizer
+    print(f"{X_train.shape=}")
     input_dim = X_train.shape[1]
     model = FeedforwardNN(input_dim).to(device)
     criterion = nn.MSELoss()
@@ -172,7 +174,9 @@ def train(prepared_data_path: Path):
         model.train()
         for epoch in range(params["epochs"]):
             epoch_loss = 0
-            for features, targets in train_loader:
+            for features, targets in tqdm(
+                train_loader, desc=f"Training Epoch {epoch + 1}/{params['epochs']}"
+            ):
                 features, targets = features.to(device), targets.to(device)
                 optimizer.zero_grad()
                 outputs = model(features).squeeze()
@@ -185,6 +189,8 @@ def train(prepared_data_path: Path):
                 f"Epoch {epoch + 1}/{params['epochs']}, Loss: {epoch_loss / len(train_loader):.4f}"
             )
 
+            eval_loss = evaluate_model(model=model, test_loader=test_loader, device=device)
+            live.log_metric("loss/eval", eval_loss)
             # # Save best model
             # if metrics_test["acc"] > best_test_acc:
             #     torch.save(model.state_dict(), "model.pt")
@@ -193,17 +199,19 @@ def train(prepared_data_path: Path):
 
         live.log_artifact("out/models/torch_imputer.pt", type="model", name="pytorch-model")
 
-        # Evaluate the model
-        model.eval()
-        criterion = nn.MSELoss()
-        with torch.no_grad():
-            total_loss = 0
-            for features, targets in test_loader:
-                features, targets = features.to(device), targets.to(device)
-                outputs = model(features).squeeze()
-                loss = criterion(outputs, targets)
-                total_loss += loss.item()
-            print(f"Test Loss: {total_loss / len(test_loader):.4f}")
+
+def evaluate_model(model, test_loader, device):
+    model.eval()
+    criterion = nn.MSELoss()
+    with torch.no_grad():
+        total_loss = 0
+        for features, targets in tqdm(test_loader, desc="Evaluating"):
+            features, targets = features.to(device), targets.to(device)
+            outputs = model(features).squeeze()
+            loss = criterion(outputs, targets)
+            total_loss += loss.item()
+        print(f"Test Loss: {total_loss / len(test_loader):.4f}")
+    return total_loss / len(test_loader)
 
 
 if __name__ == "__main__":
