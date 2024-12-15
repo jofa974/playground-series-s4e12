6eeae9c79a00dce5516dd439f8236637d24b4508
Jonathan Faustin <faustin.jonathan@gmail.com>
2024-12-15 09:03:59 -0500

    train imputer with multiclass obj

--- insurance/train_imputer.py
+++ insurance/train_imputer.py
@@ -44,7 +44,7 @@ class TabularDataset(Dataset):
     def __init__(self, features, targets=None):
         self.features = torch.tensor(features, dtype=torch.float32)
         if targets is not None:
-            self.targets = torch.tensor(targets, dtype=torch.float32)
+            self.targets = torch.tensor(targets, dtype=torch.long)
         else:
             self.targets = torch.tensor([torch.nan] * len(features))
 
@@ -55,12 +55,15 @@ class TabularDataset(Dataset):
         return self.features[idx], self.targets[idx]
 
 
-# Define a simple feedforward neural network
 class FeedforwardNN(nn.Module):
-    def __init__(self, input_dim):
+    def __init__(self, input_dim, output_dim):
         super(FeedforwardNN, self).__init__()
         self.model = nn.Sequential(
-            nn.Linear(input_dim, 64), nn.ReLU(), nn.Linear(64, 32), nn.ReLU(), nn.Linear(32, 1)
+            nn.Linear(input_dim, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, output_dim),
         )
 
     def forward(self, x):
@@ -97,6 +100,7 @@ def train():
 
     features, target, data_pipeline = prepare_data_pipeline(input_df=df)
 
+    # TODO Stratify
     X_train, X_test, y_train, y_test = train_test_split(
         features, target, test_size=0.2, random_state=42
     )
@@ -123,8 +127,11 @@ def train():
     # Initialize the model, loss function, and optimizer
     logger.info(f"{X_train.shape=}")
     input_dim = X_train.shape[1]
-    model = FeedforwardNN(input_dim).to(device)
-    criterion = nn.MSELoss()
+    output_dim = y_train.nunique()
+    logger.info(f"Number of output classes: {output_dim}")
+    model = FeedforwardNN(input_dim=input_dim, output_dim=output_dim).to(device)
+    # criterion = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=params["lr"])
 
     logger.info("NN + optim")
@@ -133,8 +140,11 @@ def train():
         live.log_params(params)
 
         model.train()
+        plots = []
         for epoch in range(params["epochs"]):
             epoch_loss = 0
+            total = 0
+            correct = 0
             for features, targets in tqdm(
                 train_loader, desc=f"Training Epoch {epoch + 1}/{params['epochs']}"
             ):
@@ -145,32 +155,60 @@ def train():
                 loss.backward()
                 optimizer.step()
                 epoch_loss += loss.item()
-            live.log_metric("loss/train", epoch_loss / len(train_loader))
+
+                _, predicted = torch.max(outputs, 1)  # Get the class with highest score
+                total += targets.size(0)
+                correct += (predicted == targets).sum().item()
+            train_loss = epoch_loss / len(train_loader)
+            train_acc = correct / total
+
             logger.info(
-                f"Epoch {epoch + 1}/{params['epochs']}, Loss: {epoch_loss / len(train_loader):.4f}"
+                f"Epoch {epoch + 1}/{params['epochs']}, Loss: {epoch_loss / len(train_loader):.4f}, Accuracy: {correct / total:.4f} "
             )
 
-            eval_loss = evaluate_model(model=model, test_loader=test_loader, device=device)
+            eval_loss, eval_accuracy = evaluate_model(
+                model=model, test_loader=test_loader, device=device
+            )
             logger.info(f"Eval Loss: {eval_loss:.4f}")
-            live.log_metric("loss/eval", eval_loss)
+            logger.info(f"Eval Accuracy: {eval_accuracy:.4f}")
 
+            plots.append(
+                {
+                    "epoch": epoch,
+                    "train_loss": train_loss,
+                    "eval_loss": eval_loss,
+                    "train_accuracy": train_acc,
+                    "eval_accuracy": eval_accuracy,
+                }
+            )
+            live.log_plot(name="loss", datapoints=plots, x="epoch", y=["train_loss", "eval_loss"])
+            live.log_plot(
+                name="accuracy", datapoints=plots, x="epoch", y=["train_accuracy", "eval_accuracy"]
+            )
             live.next_step()
 
         torch.save(model.state_dict(), MODEL_PATH)
         live.log_artifact(MODEL_PATH, type="model", name="pytorch-model")
 
 
-def evaluate_model(model, test_loader, device):
+def evaluate_model(model, test_loader, device) -> tuple[np.float64, np.float64]:
     model.eval()
-    criterion = nn.MSELoss()
+    # criterion = nn.MSELoss()
+    criterion = nn.CrossEntropyLoss()
     with torch.no_grad():
         total_loss = 0
+        correct = 0
+        total = 0
         for features, targets in tqdm(test_loader, desc="Evaluating"):
             features, targets = features.to(device), targets.to(device)
             outputs = model(features).squeeze()
             loss = criterion(outputs, targets)
             total_loss += loss.item()
-    return total_loss / len(test_loader)
+
+            _, predicted = torch.max(outputs, 1)  # Get the class with highest score
+            total += targets.size(0)
+            correct += (predicted == targets).sum().item()
+    return total_loss / len(test_loader), correct / total
 
 
 if __name__ == "__main__":
