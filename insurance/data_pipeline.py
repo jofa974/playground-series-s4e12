6be37268436078e64416b16ba657538c45900f07
Jonathan Faustin <faustin.jonathan@gmail.com>
2024-12-24 15:23:24 -0500

    Refact: train xgboost with cv and save models

--- insurance/data_pipeline.py
+++ insurance/data_pipeline.py
@@ -50,13 +50,11 @@ def get_feat_columns():
         "Education Level",
         "Occupation",
         "Location",
-        "Policy Type",
-        "Customer Feedback",
         "Smoking Status",
         "Exercise Frequency",
         "Property Type",
     ]
-    ordinal_feat_cols = []
+    ordinal_feat_cols = ["Policy Type", "Customer Feedback"]
     date_time_cols = ["Policy Start Date"]
 
     feat_cols = Features(
@@ -93,9 +91,50 @@ class DateTransformer(BaseEstimator, TransformerMixin):
         X["month"] = X[self.date_column].dt.month
         X["day"] = X[self.date_column].dt.day
         X["dayofweek"] = X[self.date_column].dt.dayofweek
+        X["Avg Claims Per Month Since Policy Started"] = X["Previous Claims"] / X[
+            self.date_column
+        ].apply(lambda x: (2024 - x.year) * 12 + 12 - x.month)
         # Drop the original date column
         X = X.drop(columns=[self.date_column])
 
+        # Return a pandas DataFrame with consistent column names
+        return X
+
+    def set_output(self, transform="default"):
+        """
+        Enable output configuration.
+        """
+        self._output_config = transform
+        return self
+
+    def _more_tags(self):
+        """
+        Add 'requires_fit' tag for compatibility with set_output.
+        """
+        return {"requires_fit": True}
+
+
+class ClaimsTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self._is_fitted = False
+
+    def fit(self, X, y=None):
+        # Store feature names for compatibility with set_output
+        self.feature_names_in_ = X.columns if hasattr(X, "columns") else None
+        self._is_fitted = True
+        return self
+
+    def transform(self, X):
+        # Check if the transformer has been fitted
+        check_is_fitted(self, attributes=["_is_fitted"])
+        X = pd.DataFrame(_safe_indexing(X, None))  # Handle different types of inputs safely
+
+        # Convert the date column to datetime
+        X.loc[X["Previous Claims"].isna(), "Previous Claims"] = X["Previous Claims"].median()
+        start_date = pd.to_datetime(X["Policy Start Date"], format="%Y%m%d")
+        X["Avg Claims Per Month Since Policy Started"] = X["Previous Claims"] / start_date.apply(
+            lambda x: (2024 - x.year) * 12 + 12 - x.month
+        )
         # Return a pandas DataFrame with consistent column names
         return X
 
@@ -136,10 +175,30 @@ def make_pipeline(feat_cols: Features | None = None, *, do_scale=True) -> Pipeli
     # No need to OH encode bc XGBoost can deal with that.
     cat_transformer = Pipeline([("imputer", SimpleImputer(strategy="most_frequent"))])
 
-    ord_transformer = Pipeline(
+    policy_ord_transformer = Pipeline(
         [
             # ("imputer", SimpleImputer(strategy="most_frequent")),
-            ("ordinal", OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1)),
+            (
+                "ordinal",
+                OrdinalEncoder(
+                    categories=[["Basic", "Comprehensive", "Premium"]],
+                    handle_unknown="use_encoded_value",
+                    unknown_value=-1,
+                ),
+            ),
+        ]
+    )
+    feedback_ord_transformer = Pipeline(
+        [
+            # ("imputer", SimpleImputer(strategy="most_frequent")),
+            (
+                "ordinal",
+                OrdinalEncoder(
+                    categories=[["Poor", "Average", "Good"]],
+                    handle_unknown="use_encoded_value",
+                    unknown_value=-1,
+                ),
+            ),
         ]
     )
     date_transformer = Pipeline(
@@ -148,6 +207,12 @@ def make_pipeline(feat_cols: Features | None = None, *, do_scale=True) -> Pipeli
         ]
     )
 
+    # claims_transformer = Pipeline(
+    #     [
+    #         ("claims_transformer", ClaimsTransformer()),
+    #     ]
+    # )
+
     transformers = []
     # if feat_cols.numeric:
     #     transformers.append(("num", numeric_transformer, feat_cols.numeric))
@@ -155,10 +220,15 @@ def make_pipeline(feat_cols: Features | None = None, *, do_scale=True) -> Pipeli
     #     transformers.append(("num_log", log_transformer, feat_cols.numeric_log))
     # if feat_cols.categorical:
     #     transformers.append(("cat", cat_transformer, feat_cols.categorical))
-    if feat_cols.ordinal:
-        transformers.append(("ord", ord_transformer, feat_cols.ordinal))
+    # if feat_cols.ordinal:
+    #     transformers.append(("ord", ord_transformer, feat_cols.ordinal))
     if feat_cols.date_time:
-        transformers.append(("date", date_transformer, feat_cols.date_time))
+        transformers.append(("date", date_transformer, ["Policy Start Date", "Previous Claims"]))
+
+    # transformers.append(("avg_claims", claims_transformer, ["Previous Claims", "Insurance Duration"]))
+    transformers.append(("policy_type", policy_ord_transformer, ["Policy Type"]))
+    transformers.append(("customer_feedback", feedback_ord_transformer, ["Customer Feedback"]))
+
     preprocessor = ColumnTransformer(
         transformers=transformers, remainder="passthrough", verbose_feature_names_out=False
     )
