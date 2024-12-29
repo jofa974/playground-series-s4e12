8e3dab0be48735819f341aed4f163764a95b7bfc
Jonathan Faustin <faustin.jonathan@gmail.com>
2024-12-28 21:35:18 -0500

    WIP

--- insurance/prepare_basic.py
+++ insurance/prepare_basic.py
@@ -1,34 +1,42 @@
 import pandas as pd
-from sklearn.utils import shuffle
-import dvc.api
+from insurance.logger import setup_logger
 from insurance.common import RAW_DATA_PATH, PREP_DATA_PATH
+from insurance.data_pipeline import get_feat_columns, make_pipeline
 import numpy as np
 
-
-def prepare(df: pd.DataFrame) -> pd.DataFrame:
-    """Cleans data."""
-    df = df.drop(columns=["id"])
-    df["Policy Start Date"] = (
-        pd.to_datetime(df["Policy Start Date"]).dt.strftime("%Y%m%d").astype(np.int64)
-    )
-    categorical_columns = df.select_dtypes(include=["object", "category"]).columns
-    for col in categorical_columns:
-        df[col] = df[col].astype("category")
-
-    return df
+logger = setup_logger(name="Train/Test Data preparation")
 
 
 def main():
     PREP_DATA_PATH.mkdir(parents=True, exist_ok=True)
 
-    params = dvc.api.params_show()["data"]
-
-    df = (
-        pd.read_csv(RAW_DATA_PATH / "train.csv")
-        # .pipe(shuffle, random_state=params["random_state"])
-        .pipe(prepare)
-        .pipe(lambda df: df.to_feather(PREP_DATA_PATH / "prepared_data.feather"))
+    df_train = pd.read_csv(RAW_DATA_PATH / "train.csv")
+    df_train = df_train.drop(columns=["id"])
+    df_train["Policy Start Date"] = (
+        pd.to_datetime(df_train["Policy Start Date"]).dt.strftime("%Y%m%d").astype(np.int64)
     )
+    logger.info("Transforming training data...")
+    feat_cols = get_feat_columns()
+    data_pipeline = make_pipeline()
+    df_train = data_pipeline.fit_transform(df_train)
+    for col in feat_cols.categorical:
+        df_train[col] = df_train[col].astype("category")
+    train_data_path = PREP_DATA_PATH / "train_data.feather"
+    df_train.to_feather(train_data_path)
+    logger.info(f"Training data saved to {train_data_path}...")
+
+    df_test = pd.read_csv(RAW_DATA_PATH / "train.csv")
+    df_test = df_test.drop(columns=["id"])
+    df_test["Policy Start Date"] = (
+        pd.to_datetime(df_test["Policy Start Date"]).dt.strftime("%Y%m%d").astype(np.int64)
+    )
+    logger.info("Transforming test data...")
+    df_test = data_pipeline.transform(df_test)
+    for col in feat_cols.categorical:
+        df_test[col] = df_test[col].astype("category")
+    test_data_path = PREP_DATA_PATH / "test_data.feather"
+    df_test.to_feather(test_data_path)
+    logger.info(f"Test data saved to {test_data_path}...")
 
 
 if __name__ == "__main__":
